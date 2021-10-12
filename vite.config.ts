16acc8cff6924d84d5a0a5652e0be43859786846
Duc <duc@hello-charles.com>
2021-10-12 13:29:17 +0200

    try without vue

--- vite.config.ts
+++ vite.config.ts
@@ -14,13 +14,13 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      // external: ["vue"],
+      external: ["vue"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
-        // globals: {
-        //   vue: "Vue",
-        // },
+        globals: {
+          vue: "Vue",
+        },
       },
     },
   },
